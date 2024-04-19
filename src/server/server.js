9c497555579a0f760afb18462c78d6d889aa96b4
Liv Åberg <143514074+LiHa23@users.noreply.github.com>
2024-04-19 18:55:41 +0200

    Add files via upload

--- src/server/server.js
+++ src/server/server.js
@@ -25,7 +25,7 @@ app.use(express.json())
 
 // Cors configuration
 app.use((req, res, next) => { // ny
-  res.header('Access-Control-Allow-Origin', 'https://software-project-joji.vercel.app/');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   // Allow preflight requests to pass through
@@ -36,7 +36,7 @@ app.use((req, res, next) => { // ny
   }
 });
 
-const allowedOrigins = [ 'https://software-project-joji.vercel.app/', 'https://software-project-joji-koqwrjht9-livs-projects-76915312.vercel.app' ] // ny
+const allowedOrigins = [ 'https://software-project-liard.vercel.app/', 'https://software-project-joji-koqwrjht9-livs-projects-76915312.vercel.app' ] // ny
 
 app.use(cors({ // ny
   origin: function (origin, callback) {
