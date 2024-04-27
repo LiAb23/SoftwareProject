2f19e749ee2bc2a416f639d8cbdb1fd49420fa8e
Liv Åberg <143514074+LiHa23@users.noreply.github.com>
2024-04-27 11:34:39 +0200

    Add files via upload

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -5,57 +5,67 @@
  * @version 1.0.0
  */
 
-import React, { useState } from 'react';
-import './AppStyles.css'; // Säkerställ att dina stilmallar är korrekt länkade
-import './AppStyles.css'
-import Board from './Board.jsx'
-import { FaSistrix, FaCog } from 'react-icons/fa'
+import React, { useState } from "react"
+import "./AppStyles.css"
+import "./AppStyles.css"
+import Board from "./Board.jsx"
+import { FaSistrix, FaCog } from "react-icons/fa"
 
 // A component
 
 // invänta svar från servern först
 
 export default function App() {
-  const [showLoginForm, setShowLoginForm] = useState(false);
+  const [showLoginForm, setShowLoginForm] = useState(false)
 
   const toggleLoginForm = () => {
-    setShowLoginForm(!showLoginForm);
-  };
+    setShowLoginForm(!showLoginForm)
+  }
 
   return (
     <div>
-    <div className="header-container">
-      <div className="picture-container"></div>
-      <h1>My noteboard</h1>
-      <div className="login-container">
-        {!showLoginForm && (
-          <button className="btn" onClick={toggleLoginForm}>
-            Login
-          </button>
-        )}
-        {showLoginForm && (
-          <div className="login-form">
-            <form>
-              <input type="text" placeholder="Username" className="login-input" />
-              <input type="password" placeholder="Password" className="login-input" />
-              <button type="button" onClick={toggleLoginForm}>Login</button>
-            </form>
-          </div>
-        )}
-      </div>
-    </div>
-    <div className="start-icons">
-      <div>
-        <FaCog className="cog-icon" />
-      </div>
-      <div className="search-form" >
-        <div className="search-icon-wrapper">
-        <FaSistrix className="search-icon" />
+      <div className="header-container">
+        <div className="picture-container"></div>
+        <h1>My noteboard</h1>
+        <div className="login-container">
+          {!showLoginForm && (
+            <button className="btn" onClick={toggleLoginForm}>
+              Login
+            </button>
+          )}
+          {showLoginForm && (
+            <div className="login-form">
+              <form>
+                <input
+                  type="text"
+                  placeholder="Username"
+                  className="login-input"
+                />
+                <input
+                  type="password"
+                  placeholder="Password"
+                  className="login-input"
+                />
+                <button type="button" onClick={toggleLoginForm}>
+                  Login
+                </button>
+              </form>
+            </div>
+          )}
         </div>
-        <input type="text" placeholder="Search" />
       </div>
+      <div className="start-icons">
+        <div>
+          <FaCog className="cog-icon" />
+        </div>
+        <div className="search-form">
+          <div className="search-icon-wrapper">
+            <FaSistrix className="search-icon" />
+          </div>
+          <input type="text" placeholder="Search" />
+        </div>
+      </div>
+      <Board />
     </div>
-    <Board />
-  </div>
   )
 }
