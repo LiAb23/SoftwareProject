50e7b3c2d3f281fa64bfa18baf782cd708487227
Liv Åberg <143514074+LiHa23@users.noreply.github.com>
2024-04-27 10:12:57 +0200

    Add files via upload

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -15,8 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <div style={{ display: 'flex', flexDirection: 'column' }}>
     <App />
 
-    <footer style={{ marginTop: 'auto', width: '100%', textAlign: 'left', padding: '10px' }}> &copy; 2024 LNU
-      </footer>
+    
     </div>
   </React.StrictMode>,
 )
