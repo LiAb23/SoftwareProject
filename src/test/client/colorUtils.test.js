f3bd1a3719473f7b46d3f5f399b5e52d3213629e
Liv Åberg <143514074+LiHa23@users.noreply.github.com>
2024-05-30 12:54:13 +0200

    Add files via upload

--- src/test/client/colorUtils.test.js
+++ src/test/client/colorUtils.test.js
@@ -1,24 +1,48 @@
-// colorUtils.test.js
-import { alternateColor } from '../../client/colorUtils'
+/**
+ * Test suite for the alternateColor function.
+ *
+ * @module colorUtilsTest
+ * @requires module:client/colorUtils
+ */
 
-describe('alternateColor', () => {
-  test('should return blue when current color is yellow', () => {
-    expect(alternateColor('yellow')).toBe('blue')
+import { alternateColor } from "../../client/colorUtils"
+
+/**
+ * Test suite for alternateColor function.
+ */
+describe("alternateColor", () => {
+  /**
+   * Test if alternateColor returns blue when current color is yellow.
+   */
+  test("should return blue when current color is yellow", () => {
+    expect(alternateColor("yellow")).toBe("blue")
   })
 
-  test('should return pink when current color is blue', () => {
-    expect(alternateColor('blue')).toBe('pink')
+  /**
+   * Test if alternateColor returns pink when current color is blue.
+   */
+  test("should return pink when current color is blue", () => {
+    expect(alternateColor("blue")).toBe("pink")
   })
 
-  test('should return yellow when current color is pink', () => {
-    expect(alternateColor('pink')).toBe('yellow')
+  /**
+   * Test if alternateColor returns yellow when current color is pink.
+   */
+  test("should return yellow when current color is pink", () => {
+    expect(alternateColor("pink")).toBe("yellow")
   })
 
-  test('should return yellow when current color is unknown', () => {
-    expect(alternateColor('unknown')).toBe('yellow')
+  /**
+   * Test if alternateColor returns yellow when current color is unknown.
+   */
+  test("should return yellow when current color is unknown", () => {
+    expect(alternateColor("unknown")).toBe("yellow")
   })
 
-  test('should throw an error if no color is provided', () => {
+  /**
+   * Test if alternateColor throws an error if no color is provided.
+   */
+  test("should throw an error if no color is provided", () => {
     expect(() => {
       alternateColor()
     }).toThrow()
